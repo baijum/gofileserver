e979290a8cd4d67270859cf298d7e52a7a1935b4
Baiju Muthukadan <baiju.m.mail@gmail.com>
2013-10-25 15:56:17 +0530

    initial commit

---
+++ README.rst
@@ -0,0 +1,17 @@
+Go File Server
+==============
+
+This program serves files in a directory.
+
+Usage::
+
+  gofileserver -port [port] -dir [directory]
+
+If directory is not specified, the current working
+directory will be served.
+
+If port number is not specified, 9999 is used
+as the default port number.
+
+The file server can be accessed using all available
+IPs or hostnames in the system.
---
+++ gofileserver.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+    "log"
+    "net/http"
+    "flag"
+    "strconv"
+    "os"
+)
+
+var port = flag.Int("port", 9999, "port number")
+var dir = flag.String("dir", "NaD", "port number")
+
+func main() {
+    flag.Parse()
+    wd, _ := os.Getwd()
+    if *dir != "NaD" {
+        wd = *dir
+    }
+
+    fileinfo, fileerr := os.Stat(wd)
+
+    if fileerr != nil {
+        if os.IsNotExist(fileerr) {
+            log.Fatal("Error: ", fileerr)
+        } else {
+            log.Fatal(fileerr)
+        }
+    }
+
+    if fileinfo.IsDir() {
+        log.Println("Serving directory: " + wd)
+    } else {
+        log.Fatal("Not a directory: " + wd)
+    }
+
+    http.Handle("/",
+        http.StripPrefix("/", http.FileServer(http.Dir(wd))))
+    log.Println("Listening port: " + strconv.Itoa(*port))
+    err := http.ListenAndServe(":" + strconv.Itoa(*port), nil)
+    if err != nil {
+        log.Fatal("ListenAndServe: ", err)
+    }
+}
